ebcce6d6e39c467e6bb11e4d4d14b27e8aeffeb5
Lewis <lewis@spellcraft.org>
2024-08-10 21:12:55 -0700

    chore: FORCE_COLOR for tests

--- test/ansi.test.ts
+++ test/ansi.test.ts
@@ -1,15 +1,23 @@
-import { expect, test, describe } from "bun:test"
+import { expect, test, describe, beforeAll, afterAll } from "bun:test"
 import { createParser, endParser, writeToParser } from "@/parser"
 import { createANSIRenderer } from "@/renderer/ansi"
 import { readFile } from "fs/promises"
+
+beforeAll(() => {
+  process.env.FORCE_COLOR = "1"
+})
+
+afterAll(() => {
+  delete process.env.FORCE_COLOR
+})
 
 describe("Streaming Markdown Parser", () => {
   test("should correctly parse and render markdown", async () => {
     let source = await readFile("readme.md", "utf8")
 
     // by character 1900 we cannot reproduce from snapshot
-    // source = source.slice(0,1800)
-    source = source.slice(0,1000)
+    source = source.slice(0,1800)
+    // source = source.slice(0,1000)
 
     const renderer = createANSIRenderer()
     const parser = createParser(renderer)
